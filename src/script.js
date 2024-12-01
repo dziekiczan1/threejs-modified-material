1b29797f2345045227d1767eb91383fa6ebb84cb
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-12-01 11:09:20 +0100

    fix the core shadow

--- src/script.js
+++ src/script.js
@@ -77,55 +77,72 @@ const customUniforms = {
     uTime: { value: 0 }
 }
 
-material.onBeforeCompile = (shader) => {
-    shader.uniforms.uTime = customUniforms.uTime;
+material.onBeforeCompile = (shader) =>
+{
+    shader.uniforms.uTime = customUniforms.uTime
+
     shader.vertexShader = shader.vertexShader.replace(
         '#include <common>',
         `
             #include <common>
-            
+
             uniform float uTime;
-            
-            mat2 get2dRotateMatrix(float _angle) {
-                return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+
+            mat2 get2dRotateMatrix(float _angle)
+            {
+                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
             }
         `
     );
+
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <beginnormal_vertex>',
+        `
+            #include <beginnormal_vertex>
+
+            float angle = (sin(position.y + uTime)) * 0.4;
+            mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+            objectNormal.xz = rotateMatrix * objectNormal.xz;
+        `
+    );
+
     shader.vertexShader = shader.vertexShader.replace(
         '#include <begin_vertex>',
         `
             #include <begin_vertex>
-            
-            float angle = (position.y + uTime) * 0.9;
-            mat2 rotateMatrix = get2dRotateMatrix(angle);
-            
+
             transformed.xz = rotateMatrix * transformed.xz;
         `
     );
 };
 
-depthMaterial.onBeforeCompile = (shader) => {
+depthMaterial.onBeforeCompile = (shader) =>
+{
     shader.uniforms.uTime = customUniforms.uTime;
+
     shader.vertexShader = shader.vertexShader.replace(
         '#include <common>',
         `
             #include <common>
-            
+
             uniform float uTime;
-            
-            mat2 get2dRotateMatrix(float _angle) {
-                return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+
+            mat2 get2dRotateMatrix(float _angle)
+            {
+                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
             }
         `
     );
+
     shader.vertexShader = shader.vertexShader.replace(
         '#include <begin_vertex>',
         `
             #include <begin_vertex>
-            
-            float angle = (position.y + uTime) * 0.9;
+
+            float angle = (sin(position.y + uTime)) * 0.4;
             mat2 rotateMatrix = get2dRotateMatrix(angle);
-            
+
             transformed.xz = rotateMatrix * transformed.xz;
         `
     );
